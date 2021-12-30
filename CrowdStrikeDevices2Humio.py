da3560d36b371edcd987e4fdc15b0b7f8a50d8e3
Gabe Alford <redhatrises@gmail.com>
2021-12-30 14:15:54 -0700

    Autopep8 the python files

--- CrowdStrikeDevices2Humio.py
+++ CrowdStrikeDevices2Humio.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#python imports
+# python imports
 from falconpy.hosts import Hosts
 from falconpy.oauth2 import OAuth2
 from falconpy.api_complete import APIHarness
@@ -8,7 +8,7 @@ import json
 import logging
 import sys
 
-#local imports
+# local imports
 from Send2HumioHEC import Send_to_HEC as humio
 import CrowdStrikeDevices2HumioConfig as config
 
@@ -21,8 +21,9 @@ def main():
     log_level = config.CS_devices_log_level
     proxy_used = config.CS_devices_proxy
     proxies = config.CS_devices_proxies
-    
-    logging.basicConfig(filename=log_file, filemode='a+', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=log_level)
+
+    logging.basicConfig(filename=log_file, filemode='a+',
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=log_level)
     logging.info('Devices2Humio v' + version + ': CrowdStrike Devices to Humio is starting')
 
     if proxy_used == False:
@@ -30,15 +31,16 @@ def main():
     elif proxy_used == True:
         falcon = APIHarness(client_id=clientID, client_secret=secret, proxy=proxies)
 
-    kwargs = {'clientID':clientID, 'secret':secret, 'falcon':falcon, 'log_file':log_file, 'log_level':log_level, 'version':version, 'proxy':proxy_used, 'proxies':proxies}
+    kwargs = {'clientID': clientID, 'secret': secret, 'falcon': falcon, 'log_file': log_file,
+              'log_level': log_level, 'version': version, 'proxy': proxy_used, 'proxies': proxies}
     process_list = get_device_ids(**kwargs)
-    kwargs ['process_list'] = process_list
+    kwargs['process_list'] = process_list
     logging.info('Devices2Humio v' + version + ': Getting device details')
     device_details = get_device_details(**kwargs)
     num_devices = len(device_details)
 
     for details in device_details:
-        details_str = "\n".join(map(str,details))
+        details_str = "\n".join(map(str, details))
         details_str = details_str.replace("True", "true")
         details_str = details_str.replace("False", "false")
         details_str = details_str.replace("None", "[]")
@@ -50,11 +52,13 @@ def main():
 
         logging.info('Devices2Humio v' + version + ': Sending details about ' + str(num_devices) + ' devices to Humio HEC')
         humio.send_to_HEC(details_json, num_devices)
-        
-        try: 
-            sys.exit('Devices2Humio v' + version + ': Data collection has successfully completed, CrowdStrike Device Data 2 Humio is now exiting.')
+
+        try:
+            sys.exit('Devices2Humio v' + version +
+                     ': Data collection has successfully completed, CrowdStrike Device Data 2 Humio is now exiting.')
         except:
             pass
+
 
 def authentication(**kwargs):
     clientID = kwargs.get('clientID')
@@ -64,7 +68,8 @@ def authentication(**kwargs):
     log_file = kwargs.get('log_file')
     log_level = kwargs.get('log_level')
 
-    logging.basicConfig(filename=log_file, filemode='a+', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=log_level)
+    logging.basicConfig(filename=log_file, filemode='a+',
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=log_level)
 
     payload = {"client_id": clientID, "client_secret": secret}
     logging.info('Devices2Humio v' + version + ': Attempting to retrieve OAuth2 token from CrowdStrike')
@@ -77,16 +82,17 @@ def authentication(**kwargs):
 
     return access_token
 
+
 def get_device_ids(**kwargs):
     falcon = kwargs.get('falcon')
-    device_ids=[]
+    device_ids = []
     process_list = []
 
     PARAMS = config.PARAMS
 
     response = falcon.command("QueryDevicesByFilter", parameters=PARAMS)
     pagination = response['body']['meta']['pagination']
- 
+
     total_id = pagination['total']
     offset = pagination['offset']
     response_ids = response['body']['resources']
@@ -97,12 +103,12 @@ def get_device_ids(**kwargs):
         paginate = True
     else:
         paginate = False
-    
+
     while paginate == True:
         if remaining_ids > 0:
-            PARAMS["offset"]=offset
+            PARAMS["offset"] = offset
         else:
-            PARAMS["offset"]=offset
+            PARAMS["offset"] = offset
             paginate = False
         response = falcon.command("QueryDevicesByFilter", parameters=PARAMS)
         pagination = response['body']['meta']['pagination']
@@ -114,9 +120,8 @@ def get_device_ids(**kwargs):
             device_ids.append(id)
     process_len = len(device_ids)
 
-
     while process_len > 0:
-        print( "Process list length: " + str(process_len))
+        print("Process list length: " + str(process_len))
         if process_len >= 400:
             process_ids = device_ids[0:400]
             process_list.append(process_ids)
@@ -141,6 +146,7 @@ def get_device_ids(**kwargs):
 
     return process_list
 
+
 def get_device_details(**kwargs):
     falcon = kwargs.get('falcon')
     process_list = kwargs.get('process_list')
@@ -152,9 +158,9 @@ def get_device_details(**kwargs):
         print('Details for ' + str(len(detail_info)) + ' devices will be added.')
 
         device_details.append(detail_info)
-    
-    return device_details #device_info_1 #
-    
+
+    return device_details  # device_info_1 #
+
+
 if __name__ == "__main__":
     main()
-
--- CrowdStrikeDevices2HumioConfig.py
+++ CrowdStrikeDevices2HumioConfig.py
@@ -1,66 +1,60 @@
 #!/usr/bin/env python
 
-#python imports
+# python imports
 import logging
 import datetime
 from datetime import timedelta
 
-#Set Logging level and file name
+# Set Logging level and file name
 CS_devices_log_level = logging.DEBUG
 log_file = 'CrowdStrikeDevices2Humio.log'
 
-#Code version - do not alter
+# Code version - do not alter
 CS_devices_version = '1.0'
 
-#Proxy config
+# Proxy config
 CS_devices_proxy = False
 CS_devices_proxies = {}
-#Example of Proxy syntex {"http": "http://myproxy:8888", "https": "https://myotherproxy:8080"}
+# Example of Proxy syntex {"http": "http://myproxy:8888", "https": "https://myotherproxy:8080"}
 
-#####CrowdStrike Devices Configuration
+# CrowdStrike Devices Configuration
 
-#use a timedelta to create a time in the past to populate the 'last_seen' filter
+# use a timedelta to create a time in the past to populate the 'last_seen' filter
 current_time = datetime.datetime.now()
 look_back = current_time - timedelta(hours=2)
 last_seen = look_back.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-#CrowdStrike API Filters
-PARAMS = {'limit':2000, 'filter':"last_seen:>'" + last_seen +"'"}
+# CrowdStrike API Filters
+PARAMS = {'limit': 2000, 'filter': "last_seen:>'" + last_seen + "'"}
 
-#CS_devices_limit = 
-#CS_devices_filter = "&filter=last_seen:>'2021-10-05T00:00:12Z'" #Time may be configured but needs to remain in single quotation marks
+# CS_devices_limit =
+# CS_devices_filter = "&filter=last_seen:>'2021-10-05T00:00:12Z'" #Time may be configured but needs to remain in single quotation marks
 
-#CrowdStrike API credential with Devices Scope
-CS_devices_client_id=""
-CS_devices_client_secret=""
+# CrowdStrike API credential with Devices Scope
+CS_devices_client_id = ""
+CS_devices_client_secret = ""
 
-#The base URL for CrowdStrike cloud to connect to, this URL can be found in the Falcon UI on the API client page
+# The base URL for CrowdStrike cloud to connect to, this URL can be found in the Falcon UI on the API client page
 CS_devices_base_url = 'https://api.crowdstrike.com'
 
-#CrowdStrike API endpoints 
+# CrowdStrike API endpoints
 CS_devices_tokenURL = "/oauth2/token"
 CS_devices_scroll_url = "/devices/queries/devices-scroll/v1"
 CS_devices_url = "/devices/entities/devices/v1?"
 
-#####Humio HEC configuration
+# Humio HEC configuration
 
-#Humio URL
+# Humio URL
 HumioBaseURL = ''
-HumioHECurl = HumioBaseURL+'/api/v1/ingest/hec/raw'  
-#example of a full URL: http://192.168.0.220:8080/api/v1/ingest/hec/raw
+HumioHECurl = HumioBaseURL+'/api/v1/ingest/hec/raw'
+# example of a full URL: http://192.168.0.220:8080/api/v1/ingest/hec/raw
 
 
-#Humio HEC Token
+# Humio HEC Token
 HumioHECtoken_Devices = ''
 
-#Header Content Type
-HumioHECcontent_Devices  = "'application/json', 'Accept':'application/json'"
+# Header Content Type
+HumioHECcontent_Devices = "'application/json', 'Accept':'application/json'"
 
-#Certficate validation - should only be set to false in a controlled test environment 
+# Certficate validation - should only be set to false in a controlled test environment
 HumioHECverify = True
-
-
-
-
-
-
